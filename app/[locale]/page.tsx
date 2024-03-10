cb4273b75b7dd6c5fe6b3e8f412e6683d4cdf00c
marc <hornil6748@gmail.com>
2024-03-10 15:09:53 -0400

    feature/ install initial next-intl for language support

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
+  const t = useTranslations('Index');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="bg-red-500 p-5 text-white">Hello, my name is Bob</div>
+        <div>
+          <h1>{t('title')}</h1>;
+        </div>
       </div>
     </main>
   );
